1e238deeafbb588404bf48fced04be7775891525
noelru94 <noelru94@gmail.com>
2019-08-14 13:19:12 -0400

    promptUser() asks user for id and and how many units of the product they would like to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     port: 3306,
@@ -8,19 +9,43 @@ const connection = mysql.createConnection({
 
 showItems();
 
+// when user runs the app a table will appear with 
+//items and its info 'id#,product name, and price'
 function showItems(){
+    
     connection.connect(function(err){
         if(err) throw err;
-        console.log('Connection id: ' + connection.threadId);
+        console.log(`Connection id: ${connection.threadId}\n-------------------------` );
 
         connection.query('SELECT item_id,product_name,price FROM products',function(err,res){
             if(err) throw err;
+            console.log('Welcome to Bamazon\n-------------------------');
             console.table(res);
+            promptUser();
         })
 
         connection.end();
+        
+    })
+}
+
+//
+function promptUser(){
+    inquirer.prompt([
+        {
+            message: 'Choose an ID of the product you would like to buy',
+            name: 'id'
+        },
+        {
+            message: 'How many units of the product would you like to buy?',
+            name: 'units'
+        }
+    ]).then(function(answers){
+        console.log(answers.id,answers.units);
     })
 }
 
 
-
+function checkInventory(id,units){
+    connection
+}
