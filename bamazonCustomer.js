bff11c2ff0b9d1f45ec6d1d0a16c2d1af485e1db
noelru94 <noelru94@gmail.com>
2019-08-14 08:29:57 -0400

    showItems() 'items_id,product_name,price'

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,21 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+showItems();
 
+function showItems(){
+    connection.connect(function(err){
+        if(err) throw err;
+        console.log('Connection id: ' + connection.threadId);
+
+        connection.query('SELECT item_id,product_name,price FROM products',function(err,res){
+            if(err) throw err;
+            console.table(res);
+        })
+
+        connection.end();
+    })
+}
 
 
 
