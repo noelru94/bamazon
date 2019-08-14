3b8903b630795a48fb1603b2d2265f905330d76a
noelru94 <noelru94@gmail.com>
2019-08-14 07:42:55 -0400

    seed products table

--- bamazon.sql
+++ bamazon.sql
@@ -8,6 +8,6 @@ CREATE TABLE products(
     item_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
-    price DECIMAL(5,2),
-    stock_quantity INTEGER(6)
+    price DECIMAL(5,2) NOT NULL,
+    stock_quantity INTEGER(6) NOT NULL
 );
