a17177444fef5a4113a420f7c76f5506c90b7349
noelru94 <noelru94@gmail.com>
2019-08-14 22:41:45 -0400

    total() shows total cost of purchase(s)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,12 +11,11 @@ bamazon();
 
 // when user runs the app a table will appear with 
 //items and its info 'id#,product name,and price'
-function bamazon(){
-    
-    connection.connect(function(err){
+function bamazon (){
+    connection.connect((err)=>{
         if(err) throw err;
 
-        connection.query('SELECT item_id,product_name,price FROM products',function(err,res){
+        connection.query('SELECT item_id,product_name,price FROM products',(err,res)=>{
             if(err) throw err;
             console.log('Welcome to Bamazon\n-------------------------');
             console.table(res);
@@ -26,7 +25,7 @@ function bamazon(){
 }
 
 
-function promptUserForPurchase(){
+var promptUserForPurchase = ()=>{
     inquirer.prompt([
         {
             message: 'Choose an ID number of the product you would like to buy',
@@ -36,35 +35,43 @@ function promptUserForPurchase(){
             message: 'How many units of the product would you like to buy?',
             name: 'units'
         }
-    ]).then(function(answers){
+    ]).then((answers)=>{
         checkInventory(answers.id,answers.units);
     })
 }
 
-// 
-function checkInventory(id,numOfUnitsUserWants){
-  
+
+var checkInventory = (id,numOfUnitsUserWants)=>{
     console.log(`Connection id: ${connection.threadId}`);
 
-    connection.query('SELECT stock_quantity FROM products WHERE item_id=?',[id],function(err,res){
+    connection.query('SELECT stock_quantity FROM products WHERE item_id=?',[id],(err,res)=>{
         if(err) throw err;
         var itemQuantity = res[0].stock_quantity;
             
-        if(itemQuantity > numOfUnitsUserWants){
-                
+        if(itemQuantity > numOfUnitsUserWants){    
             placeOrder(itemQuantity,numOfUnitsUserWants,id);
-
         }else if(itemQuantity < units || itemQuantity === 0){
-            console.log('we dont have enough');
+            console.log('Insufficient quantity!');
         }
     })      
 }
 
-function placeOrder(numDatabase,numUserWants,id){
+var placeOrder = (numDatabase,numUserWants,id)=>{
     connection.query('UPDATE products SET stock_quantity = ? WHERE item_id=?',[numDatabase-numUserWants,id],function(err,res){
         if(err) throw err;
         console.log('order placed');
         console.log(`database updated`);
+
+        total(numUserWants,id);
+    })
+}
+
+var total = (numOfUnits,id)=>{
+    connection.query('SELECT price FROM products WHERE item_id=?',[id],(err,res)=>{
+        if(err) throw err;
+        var unitPrice = res[0].price;
+        var total = unitPrice * numOfUnits;
+        console.log(`Your total comes out to ${total}`);
 
         connection.end();
     })
