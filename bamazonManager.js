351e0f3b26011c3b6a0eb96211e35ee5a80afac1
noelru94 <noelru94@gmail.com>
2019-08-16 07:17:52 -0400

    manager can add product, inventory,view products and view low inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,10 +7,10 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-Manager();
+ManagerPortal();
 
-function Manager(){
-    connection.connect(function(err){
+function ManagerPortal(){
+    connection.connect((err)=>{
         if(err) throw err;
         console.log('connection id: ' + connection.threadId);
 
@@ -43,6 +43,8 @@ function Manager(){
         })
     })
 }
+
+// watch connection.end
 let viewProducts = ()=> {
     console.log('view products window');
     connection.query('SELECT item_id,product_name,price,stock_quantity From products',(err,data)=>{
@@ -55,18 +57,79 @@ let viewProducts = ()=> {
 
 let viewLowInventory = ()=> {
     console.log('view low inventory window');
+    connection.query('SELECT item_id,product_name,price From products WHERE stock_quantity < ?',['5'],(err,data)=>{
+        if(err) throw err;
 
+        console.table(data);
+    })
     connection.end();
 }
 
 let addToInventory = ()=> {
     console.log('add to inventory');
+    connection.query('SELECT item_id,product_name,price,stock_quantity From products',(err,data)=>{
+        if(err) throw err;
+        console.table(data);
+    
+        inquirer.prompt([
+            {
+                type: 'number',
+                message:'Which product would you like to add inventory too?',
+                name: 'itemId'
+            },
+            {
+                type: 'number',
+                message: 'Add quantity to inventory',
+                name: 'quantity'
+            }
+        ]).then((answer)=>{
+        
+            var newInventoryTotal = data[answer.itemId-1].stock_quantity + answer.quantity;
 
-    connection.end();
+            connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?',[newInventoryTotal,answer.itemId],(err,data)=>{
+                if(err) throw err;
+                connection.end();
+            })
+        })
+    });
 }
 
 let addNewProduct = ()=> {
     console.log('add new product');
+    connection.query('SELECT item_id,product_name,price,stock_quantity From products',(err,data)=>{
+        if(err) throw err;
+        console.table(data);
 
-    connection.end();
+        inquirer.prompt([
+            {
+                input: 'number',
+                message: 'input ID number',
+                name: 'item_id'
+            },
+            {
+                message: 'Name of item',
+                name:'product_name'
+            },
+            {
+                message: 'Department name',
+                name: 'department'
+            },
+            {
+                input: 'number',
+                message: 'Price of item',
+                name: 'price'
+            },
+            {
+                input: 'number',
+                message: 'Stock quantity',
+                name:'stock_quantity'
+            }
+        ]).then((answer)=>{
+            connection.query('INSERT INTO products(item_id,product_name,department_name,price,stock_quantity)Values(?,?,?,?,?)',
+            [answer.item_id,answer.product_name,answer.department,answer.price,answer.stock_quantity],(err)=>{
+                if(err) throw err;
+                connection.end();
+            })
+        })
+    })
 }
